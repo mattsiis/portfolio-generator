830fc25d7c52858ffa7613f0bad8d3b137151a3c
mattsiis <35969076+mattsiis@users.noreply.github.com>
2021-08-06 22:14:49 -0400

    first inquirer

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'what is your name?'
+        }
+    ])
+    .then(answers => console.log(answers))
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw new Error(err);
 
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log('Portfolio complete!')
-})
+//     console.log('Portfolio complete!')
+// })
